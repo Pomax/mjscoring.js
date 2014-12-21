e7a14eba6d0647acc71da371492b30b594c4b4c1
Pomax <pomax@nihongoresources.com>
2014-12-20 17:00:02 -0800

    uglify

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var react = require('gulp-react');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('default', function () {
   gulp.src('react/**/*.jsx')
       .pipe(react())
       .pipe(gulp.dest('build'))
-      .pipe(concat('pack.js'))
+      .pipe(concat('packed.js'))
+      .pipe(uglify())
       .pipe(gulp.dest('dist'))
 });
