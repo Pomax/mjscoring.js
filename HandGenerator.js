b9a5024306c003ef24da22a8f78610ee3b0d0f57
Pomax <pomax@nihongoresources.com>
2014-12-23 13:25:14 -0800

    handgenerator

--- HandGenerator.js
+++ HandGenerator.js
@@ -13,8 +13,8 @@ HandGenerator.prototype = {
   odds: {
     // set types
     chow: 1,
-    pung: 1,
-    kong: 1,
+    pung: 0.1,
+    kong: 0.01,
     // face type
     numeric: 1,
     simple: 1,
@@ -26,7 +26,9 @@ HandGenerator.prototype = {
     // hand type
     suited: 1,
     nohonours: 1,
-    fullconceal: 1
+    fullconceal: 1,
+    // bonusses
+    bonus: 0.1
   },
 
   tileNames: [
@@ -82,24 +84,30 @@ HandGenerator.prototype = {
 
   generate: function() {
     this.resetAvailableTiles();
-    this.players.forEach(function(p) {
-      players.setHand(this.generateHand());
+    var hands = this.players.map(function(player) {
+      var hand = this.generateHand();
+      player.setHand(hand);
+      return hand;
     }.bind(this));
+    return hands;
   },
 
   generateHand: function() {
-    var bonus=[];
-    var rchance = 1/144;
     // award bonus tiles
-    for(i=0; i<8; i++) {
-      bonus[i] = Math.random() < rchance;
+    var bonus=[];
+    for(var i=0; i<8; i++) {
+      if(this.tiles[34+i] == 0) {
+        bonus[i] = false;
+      }
+      bonus[i] = Math.random() < this.odds.bonus;
+      if(bonus[i]) { this.tiles[34+i]--; }
     }
-    // how many sets?
-    var setCount = Math.round(5*Math.random());
-    // award sets
+    // how many sets? we want a number with "higher" bias
+    var setCount = Math.round(Math.sqrt(25*Math.random()));
     var sets = []
-    for(i=0;i<setCount;i++) {
-      set.push(this.generateSet(i===4 ? "pair" : false));
+    for(var i=0; i<setCount; i++) {
+      var set = this.generateSet(i===4 ? "pair" : false);
+      sets.push(set);
     }
     return { bonus: bonus, sets: sets };
   },
@@ -117,14 +125,43 @@ HandGenerator.prototype = {
     var sequential = (settype==="chow");
     var start = 0;
     var end = sequential? 3*9 : this.tileNames.length-8;
-    var tile = start + (Math.random() * (end-start);
+    var tile = start + ((Math.random() * (end-start))|0);
 
-    if(this.tiles[tile]<tilecount) {
-      // we'll have to look for an alternate tile in the start-end range
+    if(this.tiles[tile] < tilecount || (sequential && tile > 3*9-2)) {
+      (function(tilecount) {
+        // FIXME: we don't want to always start at 0, we want
+        // to start at tile and fan out instead.
+        for(tile=0; tile<(sequential?Math.min(16,end) : end); tile++) {
+          if(sequential) {
+            if(this.tiles[tile]===0 || this.tiles[tile+1]===0 || this.tiles[tile+2]===0
+               || this.tiles[tile]%9 > this.tiles[tile+1] % 9 || this.tiles[tile+1] % 9 > this.tiles[tile+2] %9)
+                 continue;
+          }
+          else if(this.tiles[tile]<tilecount) continue;
+          break;
+        }
+      }.bind(this)(settype === "chow" ? 1 : 3));
     }
 
     var tiles = [];
-    while(tilecount-->0) { tiles.push(tile); }
-    return { tiles: tiles };
+    for(i=0; tilecount-->0; i++) {
+      tiles.push(sequential ? tile+i : tile);
+      this.tiles[sequential ? tile+i : tile]--;
+    }
+
+    return this.generateSetString(tiles);
+  },
+
+  generateSetString: function(tiles) {
+   console.log(tiles);
+    var suit = false, tm=0, tile=tiles[0];
+         if(tile<9)     { suit = "c"; }
+    else if(tile<(9*2)) { suit = "b"; tm=9; }
+    else if(tile<(9*3)) { suit = "d"; tm=18; }
+    else if(tile<this.tileNames.length) { tm = 18; }
+    tiles = tiles.map(function(v) { return v - tm; });
+   console.log(tiles);
+    return (suit?suit+".":'') + tiles.join('');
   }
+
 };
